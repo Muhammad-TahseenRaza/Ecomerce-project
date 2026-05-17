7172a6912045b1b5161f584efc89a9593565fe5f
Muhammad Tahseen Raza <razamuhammadtahseen234@gmail.com>
2026-05-17 17:21:29 +0500

    remove hamza name

--- footer.js
+++ footer.js
@@ -3,7 +3,7 @@ const footerHtml =
         <div class="footer-container container grid grid-four--cols">
             <div class="content_1">
                 <img src="public/images/logo.png" alt="logo">
-                <p>Welcome to hamza ecomstore, your ultimate destination for cutting-edge gadgets!</p>
+                <p>Welcome to ecomstore, your ultimate destination for cutting-edge gadgets!</p>
             </div>
             <div class="content_2">
                 <h4>Shopping</h4>
